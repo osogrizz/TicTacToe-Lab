dfb948d0b0494dbe3cbf36deaa86e9e44e9afc37
Leo Torres <leo.htor@gmail.com>
2015-01-18 12:55:02 -0800

    version 2

--- app.js
+++ app.js
@@ -1,31 +1,62 @@
-
+// ensures I load my js to page n.b. window.onload = start.
 function start() {
+// grabs my table cells/boxes and loops while listening for 'click' event.
 var box = document.getElementsByTagName("td");
 for (var i = 0; i < box.length; i++) {
 	box[i].addEventListener("click", clickBox);	
-}
+	}
 };
-
+// determines the value of my event.taget ,if empty "" .
+// If so then innerHTML = cuurent turn value.
+// Last switchTurn runs and switches the value. 
 function clickBox(event) {
 	if (event.target.innerText === "" ) {	
 	event.target.innerHTML = turn;
 	switchTurn();
   }
 };
+// function that alternates my content ie "X" and "O".
+// Needs var turn, which is a global variable to work. 
+// Also handles my player turn notification.
 
 function switchTurn() {
-	if (turn === "x") {
-		turn="o"; 
+	var playerBoxX = document.getElementById("playerX");
+    var playerBoxO = document.getElementById("playerO");
+	if (turn === "X") {
+		turn="O"; 
+		playerBoxO.style.visibility = "visible";
+		playerBoxX.style.visibility = "hidden";
 	} else {
-		turn = "x";
+		turn = "X";
+		playerBoxX.style.visibility = "visible";
+		playerBoxO.style.visibility = "hidden";
 	}
 }
 
 
-var turn = "x";
+// global var for event.target content.
+var turn = "X";
+
+// Player turn
 
 
 
+// Option to rset the game/page at any time.Do I need
+
+
+
+// I need a way to determine if a player has won!
+
+// function winner () {
+// 	if () {
+// 		player = "X wins!";
+// 	}  else {
+// 		player = "O wins!"
+// 	}
+// }
+
+
+// var eigthFoldPath = ["box1","box2","box3"], ["box4",]
 
 
 
